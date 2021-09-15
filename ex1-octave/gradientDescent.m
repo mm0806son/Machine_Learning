60cb5bdddf21c70dc0acdc34985c485883b5cc3a
mm0806son <73909893+mm0806son@users.noreply.github.com>
2021-09-16 00:41:25 +0200

    Update gradientDescent.m

--- ex1-octave/gradientDescent.m
+++ ex1-octave/gradientDescent.m
@@ -16,9 +16,9 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
+    h = X * theta;
+    theta(:,1) = theta(:,1) - alpha / m * sum(h-y)*X(:,1)
+    theta(2) = theta(2) - alpha / m * sum(h-y)*X(2,)
 
 
 
